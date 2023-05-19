10708786ded8f9a01ef1e9e8f651825748a8b25e
Balwant Mahara <maharabalwant@gmail.com>
2023-05-19 15:40:43 +0530

    Routing
    adding routing working functionality

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ const cardTitle = 'card-title';
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageURL, newsURL} = this.props;
+    let {title, description, imageURL, newsURL, author, publishDate} = this.props;
     return (
       <div>
         <div className="card" style={{padding: '10px'}}>
@@ -13,6 +13,7 @@ export class NewsItem extends Component {
             <div className="card-body">
                 <h5 className={classNames(cardTitle)}>{title?title:''}</h5>
                 <p className="card-text">{description?description.slice(0,60):''}</p>
+                <p class="card-text"><small class="text-muted">By {author} on {new Date(publishDate).toGMTString()}</small></p>
                 <a href={newsURL} className="btn btn-primary">more...</a>
             </div>
         </div>
